99a58f3156dfe5319b40fa98ed20ad18966943cb
IlhanCihan <cihanilhan.171@gmail.com>
2025-12-09 19:46:22 +0200

    Clarify vulnerability comment in run_command function
    Updated comment to clarify the purpose of the vulnerability.

--- vulnerable.py
+++ vulnerable.py
@@ -1,4 +1,4 @@
 # vulnerable.py
 import subprocess
 def run_command(cmd):
- subprocess.run(cmd, shell=True) # This is a vulnerability
+ subprocess.run(cmd, shell=True) # This is a vulnerability test
